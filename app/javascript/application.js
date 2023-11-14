a4019dc29bc4266a1916860b9ffbf6251a97ee7e
GGOAT <ggoat0618@gmail.com>
2023-11-14 23:46:15 +0900

    [2023/11/14]

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+//23-11-12 start
+//= require_tree .
+//= require jquery
+//= require jquery_ujs
+//23-11-12 end
